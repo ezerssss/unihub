a8bb0256533610e91e8632a53f779df2313a420d
hansgabriel <hansdaduya@yahoo.com>
2023-06-06 23:23:56 +0800

    [Enchancement][Hans] fix search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, TextInput, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 interface SearchProps {
   onSearch: (query: string) => void;
 }
 
-const Search = (props: SearchProps) => {
+function Search(props: SearchProps) {
   const { onSearch } = props;
   const [searchQuery, setSearchQuery] = useState<string>('');
 
@@ -17,18 +17,18 @@ const Search = (props: SearchProps) => {
   return (
     <View className="flex-row items-center justify-center bg-white rounded-full flex-1 mx-2 px-2 py-3">
       <TouchableOpacity onPress={handleSearch}>
-        <View className="mr-2">
-          <AntDesign name="search1" size={15} color="black" />
+        <View className="mr-3 ml-1">
+          <AntDesign name="search1" size={15} color="gray" />
         </View>
       </TouchableOpacity>
       <TextInput
-        className="text-black"
+        className="text-black w-10/12"
         placeholder="Search for products"
         value={searchQuery}
         onChangeText={setSearchQuery}
       />
     </View>
   );
-};
+}
 
 export default Search;
