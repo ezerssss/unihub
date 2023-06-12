fac605480b3267aac2614d8e0154d11e02c48f73
Ezra Magbanua <magbanuaezra@gmail.com>
2023-06-12 15:23:18 +0800

    [Feature][Ezra] compress image

--- components/Icons/EmojiIcon.tsx
+++ components/Icons/EmojiIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import { emojiIconTintColor } from '../constants/Emojicolor';
+import { emojiIconTintColor } from '../../constants/color';
 
 function EmojiIcon() {
   return (
