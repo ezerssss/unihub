ae5256be871c6ce12b2d279fe5f5a190c544f3d3
cezro <hyderz1218@gmail.com>
2023-06-13 21:36:17 +0800

    update

--- components/progressbar/ProgressBar.tsx
+++ components/progressbar/ProgressBar.tsx
@@ -1,22 +1,39 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
+// currently no progress field in database
+
 function ProgressBar() {
-  function progressMessage(state: number): string {
+  function progressMessage(progress: number): string {
     let msg = '';
-    if (state === 1) {
+    if (progress === 1) {
       msg = 'Waiting for seller to recieve order...';
-    } else if (state === 2) {
+    } else if (progress === 2) {
       msg = 'Waiting for a meetup...';
-    } else if (state === 3) {
+    } else if (progress === 3) {
       msg = 'Order successful, enjoy!';
     }
     return msg;
   }
 
+  function checkProgress(bar: number, progress: number): string {
+    if (bar <= progress) {
+      return 'mx-3 h-2 w-24 rounded-3xl bg-primary-300';
+    } else {
+      return 'mx-3 h-2 w-24 rounded-3xl bg-gray-200';
+    }
+  }
+
   return (
-    <View>
-      <Text className="">{progressMessage(1)}</Text>
+    <View className="my-8">
+      <Text className="me text-m self-center font-medium">
+        {progressMessage(1)}
+      </Text>
+      <View className="flex-row items-center justify-center pt-4">
+        <View className={checkProgress(1, 1)} />
+        <View className={checkProgress(2, 1)} />
+        <View className={checkProgress(3, 1)} />
+      </View>
     </View>
   );
 }
--- screens/SpecificProduct/BuyConfirmation.tsx
+++ screens/SpecificProduct/BuyConfirmation.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import useGoBack from '../../hooks/useGoBack';
 import ReturnIcon from '../../components/icons/ReturnIcon';
 import ContentWrapper from '../../components/ContentWrapper';
+import Footer from '../../components/OrderConfirmationFooter/footer';
 import UniHubIcon from '../../components/icons/UniHubIcon';
 import ProgressBar from '../../components/progressbar/ProgressBar';
+import ChatIcon from '../../components/icons/ChatIcon';
+import { Routes } from '../../enums/routes';
+import { RootNavigationProps } from '../../types/navigation';
 
-function Buy() {
+function Buy({ navigation }: RootNavigationProps) {
   const goBack = useGoBack();
+
+  function goToChat() {
+    navigation.navigate(Routes.CHAT);
+  }
 
   return (
     <ContentWrapper hasHeader={false}>
@@ -22,7 +30,29 @@ function Buy() {
           <UniHubIcon />
         </View>
         <ProgressBar />
+        <View className="h-24 w-full flex-row items-center justify-center bg-yellow-100">
+          <Text className="absolute left-8 text-base font-medium">
+            Chat your seller
+          </Text>
+          <TouchableOpacity className="absolute right-6" onPress={goToChat}>
+            <ChatIcon />
+          </TouchableOpacity>
+        </View>
+        <View className="p-8">
+          <Text className="text-s font-medium text-primary-300">
+            Meetup Details
+          </Text>
+          <View className="flex-row">
+            <Text className="pt-2 text-xs">Meetup Location:</Text>
+            <Text className="absolute right-8 text-xs">idk</Text>
+          </View>
+          <View className="flex-row">
+            <Text className="pt-2 text-xs">Preferred Meetup Times</Text>
+            <Text className="absolute right-8 text-xs">idk 2</Text>
+          </View>
+        </View>
       </View>
+      <Footer />
     </ContentWrapper>
   );
 }
