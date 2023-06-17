d38c3915842c575e63739719a97e45b0e587e01c
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-06-17 18:54:28 +0800

    general-ui-improvements

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -61,7 +61,7 @@ function Login() {
   ) : (
     <>
       <GoogleIcon />
-      <Text className="ml-2 text-white">Sign in with your school email</Text>
+      <Text className="ml-1 text-white font-medium">Sign in with your school email</Text>
     </>
   );
 
