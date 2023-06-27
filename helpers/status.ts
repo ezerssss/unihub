35055d20d3dd6947fbe6ac0423b536f8b1d7898f
Ezra Magbanua <magbanuaezra@gmail.com>
2023-06-27 22:42:55 +0800

    [Adjustment][Ezra] status text

--- helpers/status.ts
+++ helpers/status.ts
@@ -12,17 +12,6 @@ export function getStatusText(status: StatusEnum): string {
   return text;
 }
 
-export function getStatusColor(status: StatusEnum): string {
-  const textColor = match(status)
-    .with(StatusEnum.DENY, () => 'text-red-600')
-    .with(StatusEnum.CONFIRM, () => 'text-gray-400')
-    .with(StatusEnum.MEETUP, () => 'text-yellow-500')
-    .with(StatusEnum.SUCCESS, () => 'text-green-600')
-    .exhaustive();
-
-  return textColor;
-}
-
 export function getStatusSellerText(status: StatusEnum, seller: string) {
   const text = match(status)
     .with(StatusEnum.CONFIRM, () => `${seller} is reading your order.`)
