3bdf46dd18e3cc6fdcbdcff0e7214caf0c67f025
cezro <hyderz1218@gmail.com>
2023-06-27 20:40:29 +0800

    [Feature] [Louie] Redirect from Product Sold to Home after delay

--- screens/SpecificProduct/ProductSold.tsx
+++ screens/SpecificProduct/ProductSold.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import ContentWrapper from '../../components/ContentWrapper';
 import UniHubIcon from '../../components/icons/UniHubIcon';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamsList } from '../../types/navigation';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Routes } from '../../enums/routes';
 
 function ProductSold() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamsList>>();
+
+  setTimeout(() => {
+    navigation.navigate(Routes.HOME);
+  }, 2500);
+
   return (
     <ContentWrapper hasHeader={false} hasLightStatusBar={true}>
       <View className="flex-1 items-center justify-center bg-primary-400">
