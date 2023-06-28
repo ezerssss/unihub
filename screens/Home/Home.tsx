11356e0c7ad9398b01c85a9b329c82a68beccf7b
Ezra Magbanua <magbanuaezra@gmail.com>
2023-06-28 13:36:55 +0800

    [Adjustment][Ezra] home ui and collection listener

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { View, Text, ScrollView, RefreshControl } from 'react-native';
 import React, { useState, useEffect, useContext } from 'react';
 import ContentWrapper from '../../components/ContentWrapper';
-import FeatureCarousel from '../../components/carousel/FeatureCarousel';
 import ProductsCarousel from '../../components/carousel/ProductsCarousel';
 import CategoriesCarousel from '../../components/carousel/CategoriesCarousel';
 import { HomeLoading } from '../../components/loading';
@@ -11,6 +10,7 @@ import {
   doc,
   getDocs,
   limit,
+  onSnapshot,
   query,
   where,
 } from 'firebase/firestore';
@@ -23,44 +23,22 @@ import AuthWrapper from '../../components/AuthWrapper';
 import UserContext from '../../context/UserContext';
 import { RootNavigationProps } from '../../types/navigation';
 import { Routes } from '../../enums/routes';
+import Adverts from './Adverts';
 
 function Home({ navigation }: RootNavigationProps) {
   const { user } = useContext(UserContext);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [featuredProduct, setFeaturedProduct] = useState<Product | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
 
   async function handleGetData() {
     try {
       setIsLoading(true);
-      await getFeaturedProduct();
       await getCheckThisOut();
     } catch (error) {
       alert('Something went wrong with fetching the products.');
     } finally {
       setIsLoading(false);
-    }
-  }
-
-  async function getFeaturedProduct() {
-    try {
-      const productsCollectionRef = collection(db, DB.PRODUCTS);
-      const featuredProductsQuery = query(
-        productsCollectionRef,
-        where('isFeatured', '==', true)
-      );
-
-      const querySnapshot = await getDocs(featuredProductsQuery);
-
-      if (querySnapshot.empty) {
-        return;
-      }
-
-      const doc = querySnapshot.docs[0].data() as Product;
-      setFeaturedProduct(doc);
-    } catch (error) {
-      console.error('Error getting featured product:', error);
     }
   }
 
@@ -107,9 +85,22 @@ function Home({ navigation }: RootNavigationProps) {
     }
 
     handleGetData();
-  }, [user]);
+  }, []);
 
-  if (isLoading || !featuredProduct) {
+  useEffect(() => {
+    const productsCollectionRef = collection(db, DB.PRODUCTS);
+    const unsubscribe = onSnapshot(productsCollectionRef, (snapshot) => {
+      if (snapshot.empty) {
+        return;
+      }
+
+      handleGetData();
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (isLoading) {
     return <HomeLoading />;
   }
 
@@ -117,13 +108,13 @@ function Home({ navigation }: RootNavigationProps) {
     <AuthWrapper>
       <ContentWrapper className="px-0" hasLightStatusBar={true}>
         <ScrollView
-          className="bg-white"
+          className="mb-5 bg-white"
           refreshControl={
             <RefreshControl refreshing={isLoading} onRefresh={handleGetData} />
           }
         >
-          <View className="bg-secondary-400 px-4 py-8">
-            <FeatureCarousel product={featuredProduct} />
+          <View className="bg-secondary-400 p-4">
+            <Adverts />
           </View>
           <View className="max-h-max bg-white">
             <View className="mx-3 my-5 flex-1">
