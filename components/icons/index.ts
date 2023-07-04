1f1afb9f50f9b5f8018571931e41c96ea655bb57
Ezra Magbanua <magbanuaezra@gmail.com>
2023-07-04 16:52:36 +0800

    [Adjustment][Ezra] splash ui

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -4,3 +4,4 @@ export { default as ProfileIcon } from './ProfileIcon';
 export { default as ProductIcon } from './ProductIcon';
 export { default as ShopIcon } from './ShopIcon';
 export { default as SettingsIcon } from './SettingsIcon';
+export { default as UniHubLoginIcon } from './UniHubLogin';
--- components/loading/Login.tsx
+++ components/loading/Login.tsx
@@ -1,13 +1,13 @@
 import { View } from 'react-native';
 import React from 'react';
 import ContentWrapper from '../ContentWrapper';
-import UniHubIcon from '../icons/UniHubIcon';
+import { UniHubLoginIcon } from '../icons';
 
 export default function Login() {
   return (
     <ContentWrapper hasHeader={false}>
-      <View className="flex-1 items-center justify-center bg-primary-100">
-        <UniHubIcon />
+      <View className="flex-1 items-center justify-center">
+        <UniHubLoginIcon />
       </View>
     </ContentWrapper>
   );
