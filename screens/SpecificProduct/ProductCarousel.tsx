89139db843a3c7aabe72e3fadfc26d9f086cc7eb
cezro <hyderz1218@gmail.com>
2023-07-06 18:20:56 +0800

    [Feature] [Louie] Full Image Viewing in Specific Product

--- screens/SpecificProduct/ProductCarousel.tsx
+++ screens/SpecificProduct/ProductCarousel.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
 import Swiper from 'react-native-swiper';
+import ImageView from 'react-native-image-viewing';
 
 interface CarouselProps {
   images: string[];
@@ -8,17 +9,35 @@ interface CarouselProps {
 
 function ProductCarousel(props: CarouselProps) {
   const { images } = props;
+  const [isFullImage, setIsFullImage] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  function setIntoImageSource(imageUri: string) {
+    return { uri: imageUri };
+  }
 
   const renderProducts = images.map((image) => (
     <View className="overflow-hidden rounded-2xl" key={image}>
-      <View className="relative">
-        <Image
-          className="h-full w-full rounded-lg"
-          source={{
-            uri: image,
-          }}
-        />
-      </View>
+      <ImageView
+        imageIndex={imageIndex}
+        images={images.map(setIntoImageSource)}
+        visible={isFullImage}
+        onRequestClose={() => setIsFullImage(false)}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          setIsFullImage(true);
+        }}
+      >
+        <View className="relative">
+          <Image
+            className="h-full w-full rounded-lg"
+            source={{
+              uri: image,
+            }}
+          />
+        </View>
+      </TouchableOpacity>
     </View>
   ));
 
